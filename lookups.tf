2ab850bfcc3ebe9d5fc4bcb2eddd9f5b0e0aedb3
PaulJ <paulj@agylia.com>
2024-04-24 13:44:22 +0100

    Create root workspace files

---
+++ lookups.tf
@@ -0,0 +1,25 @@
+data "aws_vpc" "default" {
+  default = true
+
+  # filter {
+  #   name   = "tag:Name"
+  #   values = ["Agylia"]
+  # }
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  owners      = ["099720109477"]
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+}
